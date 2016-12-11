c3aecd56e00e754bb606123b4b8f244a6659a9e7
Paco Navarro <packarbell@gmail.com>
2016-12-11 11:42:00 +0100

    express configured 2

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var express = require('express');
 var app = express();
 var appPort = 3000;
 
-app.use('/', express.static('./app/'));
+app.use('/', express.static('./src/'));
 app.listen(appPort);
 console.log('app started in ' + appPort);
