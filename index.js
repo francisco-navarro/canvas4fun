8169aaa52034f6e95e27140f581993a414b8e833
Paco Navarro <packarbell@gmail.com>
2016-12-15 21:29:30 +0100

    BrowserSync conf

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 var express = require('express');
+var browserSync = require('browser-sync');
 var path = require('path');
 
 
-var app = express();
-var appPort = 3000;
+// var app = express();
+// var appPort = 3000;
 
-app.use('/', express.static(__dirname + '/src/'));
-app.use('/lib', express.static(__dirname + '/node_modules/'));
+// app.use('/', express.static(__dirname + '/src/'));
+// app.use('/lib', express.static(__dirname + '/node_modules/'));
 
-app.listen(appPort);
-console.log('app started in ' + appPort);
+// app.listen(appPort);
+// console.log('app started in ' + appPort);
+
+
+browserSync.init(null, {
+    browser: 'google chrome',
+    port: 7000,
+    // files: '**/*.*',
+    server: {
+        baseDir: 'src',
+        routes: {
+          '/lib': 'node_modules'
+        }
+    }
+});
